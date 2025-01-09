c0492d7c395ad6af5b6e416d049814617692f171
Illia Hlushakov <codeofcoin@gmail.com>
2025-01-09 23:07:55 +0100

    Integrated `react-native-latex` lib but its shit so change to `react-native-math-view` is needed. The package is already installed

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,38 @@
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet, Image, Button} from 'react-native';
+import {SafeAreaProvider, SafeAreaView} from "react-native-safe-area-context";
+import {JSX, ReactElement, ReactNode} from "react";
+import Latex from "react-native-latex";
+
+const API_URL = "http://sciencesoft.at/image/latexurl/image.png?dpi=200&src=";
+
+function LatexText({children}: {children: string}): JSX.Element {
+	return <Image source={{uri: API_URL+children}}></Image>
+}
 
 export default function App() {
-	<View>
-		<Text>
-		Math tutor
-		</Text>
-	</View>
+	return (<SafeAreaProvider>
+		<SafeAreaView style={styles.container}>
+			<Text style={[styles.baseText]}>
+				Math tutor
+			</Text>
+			<Button title={"Cross product"} />
+			<Latex height={60} minHeight={10}>{"\\begin{pmatrix}a b c \\end{pmatrix}"}</Latex>
+		</SafeAreaView>
+	</SafeAreaProvider>)
 }
+
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: "center",
+		alignItems: "center"
+	},
+	baseText: {
+		fontWeight: 'bold',
+		textAlign: "center"
+	},
+	innerText: {
+		color: 'red',
+	},
+});
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -35,6 +35,6 @@ export default function RootLayout() {
     //  </Stack>
     //  <StatusBar style="auto" />
     //</ThemeProvider>
-	<Stack />
+      <Stack screenOptions={{headerShown: false}} />
   );
 }
