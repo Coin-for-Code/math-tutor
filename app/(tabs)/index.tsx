9009eea7ebaabe54b93e926d7484dacd9b6d1c06
Code of coin <112759085+Coin-for-Code@users.noreply.github.com>
2025-01-14 13:51:56 +0000

    Fixed he SaveAreaView

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import { SafeAreaView } from 'react-native';
+import { SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
 import LatexRenderer from '@/components/LatexRenderer';
+import { Button, View } from 'react-native';
 
 const App: React.FC = () => {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <LatexRenderer text='\begin{pmatrix} a \\ b \end{pmatrix}' as string />
-    </SafeAreaView>
+    <SafeAreaProvider>
+      <SafeAreaView style={{ flex: 1 }}>
+        <LatexRenderer text='\begin{pmatrix} a \\ b \end{pmatrix}'/>
+        <View style={{flex:1}}><Button title='Solve the goddamn equation' /></View>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 };
 
